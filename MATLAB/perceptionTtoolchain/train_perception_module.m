eb402cc9f8e26161ab4de0cd2fe9ade40a23130a
houssamya <habbas@seas.upenn.edu>
2015-03-20 02:02:52 -0400

    minor

--- MATLAB/perceptionTtoolchain/train_perception_module.m
+++ MATLAB/perceptionTtoolchain/train_perception_module.m
@@ -3,8 +3,8 @@ addpath(folder);
 load([folder,'/BarrelMasks.mat'])
 images = dir([folder,'/*.png']);
 
-nbimages = 3;
-listPC = [2,4];%,6];
+nbimages = 30;
+listPC = [2,4,6];
 listNConnComp = [4,8];
 listShapeFeatures = [1,3];
 
@@ -169,7 +169,6 @@ for nbGMcomp=listPC
             datestr(now)
             SVMModel = fitcsvm([featuresNegClass; featuresPosClass],[-ones(size(featuresNegClass,1),1); ones(size(featuresPosClass,1),1)],...
                 'KernelFunction','rbf','Standardize',true, ...
-                'Crossval', 'on', 'KFold', knobs.nbValidationFolds, ...
                 'PredictorNames', knobs.featuresList);
             datestr(now)
             matname = ['trained','I',num2str(nbimages), '_C',num2str(nbcolors),'_PC',num2str(nbcomponentsPOI),'_NPC',num2str(nbcomponentsNonPOI),'_NF',num2str(nbShapeFeatures),'_NCC',num2str(nbconncomp),'.mat'];
