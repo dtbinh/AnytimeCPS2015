d8420251418a5075054e08bfab7cd6688a09e05b
yashpant <pant.yash@gmail.com>
2015-04-21 13:20:23 -0400

    rm quadrotor master for now

--- Platform/quadrotor_control-master/quadrotor_simulator/launch/takeoff.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-rostopic pub -1 /quadrotor/motors std_msgs/Bool 0
-rostopic pub -1 /quadrotor/controllers_manager/line_tracker_min_jerk/goal geometry_msgs/Vector3 "{x: 0.0, y: 1.0, z: 2.0}"
-rosservice call /quadrotor/controllers_manager/transition line_tracker/LineTrackerMinJerk
-rostopic pub -1 /quadrotor/motors std_msgs/Bool 1
