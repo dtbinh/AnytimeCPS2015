f7a38391b0e043ee192764e0f610b8efa925804b
yashpant <pant.yash@gmail.com>
2015-09-13 18:38:34 -0400

    Smoother sim with higher order solver, data viz

--- Pubs/Invited_IEEE_ManEtc16/simulations/analyse_simData.m
+++ Pubs/Invited_IEEE_ManEtc16/simulations/analyse_simData.m
@@ -1,4 +1,48 @@
-%analyse data
 
+
+%analyse data
+load Data.mat
 clc;
-stairs(Scheduler.time,reshape(Scheduler.signals(4).values,numel(Scheduler.time),1));
+%stairs(Scheduler.time,reshape(Scheduler.signals(2).values,numel(Scheduler.time),1));
+
+
+%which schedule
+Schedule_pick = reshape(Scheduler.signals(4).values,numel(Scheduler.time),1);
+CPUF_pick = reshape(Scheduler.signals(2).values,numel(Scheduler.time),1);
+GPUF_pick = reshape(Scheduler.signals(3).values,numel(Scheduler.time),1);
+
+%i(t),j(t),k(t) are gpu freq index, cpu freq index, schedule index at time t
+i = zeros(length(Scheduler.time),1);
+j = zeros(length(Scheduler.time),1);
+k = zeros(length(Scheduler.time),1);
+Power = zeros(length(Scheduler.time),1);
+CPU_F = zeros(length(Scheduler.time),1);
+GPU_F = zeros(length(Scheduler.time),1);
+
+for t = 1:length(Scheduler.time) % for all sim times
+    for sch = 1:size(Schedule,1) %find which CPU-GPU schedule was picked
+        if(Schedule_pick(t) == str2num(Schedule(sch,:)))
+            k(t) = sch;
+        end
+    end
+    i(t) = CPUF_pick(t);
+    j(t) = GPUF_pick(t);
+    
+    Power(t) = MeanPower(i(t),j(t),k(t));
+    CPU_F(t) = CPU_freq(i(t),1);
+    GPU_F(t) = GPU_freq(j(t),1);
+end
+
+stairs(Scheduler.time,Power)
+figure
+stairs(Scheduler.time,CPU_F)
+hold all
+stairs(Scheduler.time,GPU_F)
+figure;
+plot(SysOut.signals(3).values,SysOut.signals(4).values);
+ControlCost = sum(SysOut.signals(3).values.^2)
+kk = reshape(ScopeData.signals(3).values,numel(ScopeData.time),1);
+'Energy'
+kk(end)
+
+
--- Pubs/Invited_IEEE_ManEtc16/simulations/func_it_up.m
+++ Pubs/Invited_IEEE_ManEtc16/simulations/func_it_up.m
@@ -2,9 +2,9 @@ function [alpha] = func_it_up(vp,mp)
 
 %u(1) is VP, u(2) is MP
 %map diff u's to alpha
-deadz = 0.162; %0.152
+deadz = 0.152; %0.152 %deadzone
 I = deadzone('ZeroInterval',[-deadz deadz]);
-vp = evaluate(I,10^7*vp);
+vp = evaluate(I,10^7*vp); %scale VP to bring it close to [-1,1]
 %vp = evaluate(I,.33*10^5*mp); %mp only
 alpha = abs(vp)+0*deadz;
 if(alpha<.001)
