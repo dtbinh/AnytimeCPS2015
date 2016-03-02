f523ea61b5d7bd2c461c5298ef2a0fb7b025b9e5
Yash Vardhan Pant <yashpant@seas.upenn.edu>
2016-03-02 12:12:42 -0500

    SQP for inner approx of Z s.t. zinZ means xinX

--- Pubs/CDC16/Code/sinx_example_2state/TestOpts/confun.m
+++ Pubs/CDC16/Code/sinx_example_2state/TestOpts/confun.m
@@ -6,13 +6,20 @@ function [c, ceq] = confun(x)
 %      -2-x(1);
 %      x(2)-2;
 %      -2-x(2)];
-c = [x(1)-pi/2;
-    -x(1)-pi/2;
-    x(2)-pi/2;
-    -x(2)-pi/2;
-    x(3)-pi/4;
-    -x(3)-pi/4;
-    x(4)-pi/3;
-    -x(4)-pi/3];
+% c = [x(1)-pi/2;
+%     -x(1)-pi/2;
+%     x(2)-pi/2;
+%     -x(2)-pi/2;
+%     x(3)-pi/4;
+%     -x(3)-pi/4;
+%     x(4)-pi/3;
+%     -x(4)-pi/3];
+load('SetsAndParams.mat');
+c = X.A*x(1:4)-X.b; %x is first 4, z is next 4
+
 % Nonlinear equality constraints
-ceq = [];
+%ceq = x(1)-sin(x(2));
+ceq = [x(5) - x(1);
+    x(6) - x(2);
+    x(7) + 1*sin(x(1)) + 1*(x(1)-x(3));
+    x(8) + 1*x(2)*cos(x(1)) + 1*(x(2)-x(4))];
--- Pubs/CDC16/Code/sinx_example_2state/TestOpts/mainOpt.m
+++ Pubs/CDC16/Code/sinx_example_2state/TestOpts/mainOpt.m
@@ -1,6 +1,6 @@
 x0 = [0,0,0,0];     % Make a starting guess at the solution
 
-
+x0 = zeros(8,1);
 %options = optimoptions(@fmincon,'Algorithm','sqp');
 options = optimset('Algorithm','sqp');
 [x,fval] = ... 
