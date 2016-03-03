8d2a4738a35bed9af05c57563dae5f795392b64d
Yash Vardhan Pant <yashpant@seas.upenn.edu>
2016-03-03 14:26:46 -0500

    rmd some stuff

--- Pubs/CDC16/Code/manipulator_example/OptForConstr/mainOpt.m
+++
@@ -1,8 +0,0 @@
-x0 = [0,0,0,0];     % Make a starting guess at the solution
-
-
-%options = optimoptions(@fmincon,'Algorithm','sqp');
-options = optimset('Algorithm','sqp');
-[x,fval] = ... 
-fmincon(@objfun,x0,[],[],[],[],[],[],@confun,options)
-
