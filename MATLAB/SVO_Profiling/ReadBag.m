fd41bfc3ca17a8d367c86101239942ebd2006d08
yashpant <pant.yash@gmail.com>
2016-05-17 15:27:21 -0400

    km.launch to qc2/odom as odom and readbag.m edited

--- MATLAB/SVO_Profiling/ReadBag.m
+++ MATLAB/SVO_Profiling/ReadBag.m
@@ -1,10 +1,18 @@
-filepath = '~/bag_files/bag_files/2016-05-09/2016-05-09-15-13-27.bag';
+%run tmuxinator start ErrorComp for running shit and logging data after bag
+%file play
+
+filepath = '~/bag_files/bag_files/processed/2016-05-12/run3/2016-05-17-15-07-46.bag';
 bag = rosbag(filepath);
-svo_odom = select(bag,'Topic','/svo/odom');
-msgs  = readMessages(svo_odom);
-%Pose_position = [msgs{1}.Pose.Pose.Position.X msgs{1}.Pose.Pose.Position. Y 
-ts = timeseries(svo_odom,'Pose.Pose.Position.X','Pose.Pose.Position.Y','Pose.Pose.Position.Y');
-size(ts.data);
+
+%offset for end if needed
+off_end = 50; %2016-05-12/run2/, use 30 or above, 50 works for run3 as well
+
+
+%svo_odom = select(bag,'Topic','/svo/odom');
+%msgs  = readMessages(svo_odom);
+%Pose_position = [msgs{1}.Pose.Pose.Position.X msgs{1}.Pose.Pose.Position. Y
+%ts = timeseries(svo_odom,'Pose.Pose.Position.X','Pose.Pose.Position.Y','Pose.Pose.Position.Y');
+%size(ts.data);
 
 %get vicon pose
 Vicon_Pose = select(bag,'Topic','/vo_test/Vicon_Pose');
@@ -17,21 +25,51 @@ Vis_Pose_Pos = timeseries(Vision_Pose,'Pose.Position.X','Pose.Position.Y','Pose.
 Vis_Pose_Ori = timeseries(Vision_Pose,'Pose.Orientation.W','Pose.Orientation.X', ...
     'Pose.Orientation.Y','Pose.Orientation.Z');
 
-%plot3
+%% plot3
 figure;
-plot3(Vic_Pose_Pos.Data(:,1),Vic_Pose_Pos.Data(:,2),Vic_Pose_Pos.Data(:,3));
+plot3(Vic_Pose_Pos.Data(1:end-off_end,1),Vic_Pose_Pos.Data(1:end-off_end,2), ...
+    Vic_Pose_Pos.Data(1:end-off_end,3));
 hold all;
 grid on;
-plot3(Vis_Pose_Pos.Data(:,1),Vis_Pose_Pos.Data(:,2),Vis_Pose_Pos.Data(:,3));
-
+plot3(Vis_Pose_Pos.Data(1:end-off_end,1),Vis_Pose_Pos.Data(1:end-off_end,2), ...
+    Vis_Pose_Pos.Data(1:end-off_end,3));
+%%
 %error
-err_x = Vic_Pose_Pos.Data(:,1) - Vis_Pose_Pos.Data(:,1);
-err_y = Vic_Pose_Pos.Data(:,2) - Vis_Pose_Pos.Data(:,2);
-err_z = Vic_Pose_Pos.Data(:,3) - Vis_Pose_Pos.Data(:,3);
+err_x = Vic_Pose_Pos.Data(1:end-off_end,1) - Vis_Pose_Pos.Data(1:end-off_end,1);
+err_y = Vic_Pose_Pos.Data(1:end-off_end,2) - Vis_Pose_Pos.Data(1:end-off_end,2);
+err_z = Vic_Pose_Pos.Data(1:end-off_end,3) - Vis_Pose_Pos.Data(1:end-off_end,3);
 
-[Vis_yaw,Vis_pitch,Vis_roll] = quat2angle(Vis_Pose_Ori.data);
-[Vic_yaw,Vic_pitch,Vic_roll] = quat2angle(Vic_Pose_Ori.data);
+[Vis_yaw,Vis_pitch,Vis_roll] = quat2angle(Vis_Pose_Ori.data(1:end-off_end,:));
+[Vic_yaw,Vic_pitch,Vic_roll] = quat2angle(Vic_Pose_Ori.data(1:end-off_end,:));
 
 err_yaw = Vis_yaw-Vic_yaw;
 err_pitch = Vis_pitch-Vic_pitch;
-err_roll = Vis_roll-Vic_roll;
+err_roll = Vis_roll-(Vic_roll);
+
+%% plot hists
+if(1)
+    subplot(321)
+    histogram(err_yaw-mean(err_yaw))
+    %title('yaw')
+    xlabel('yaw err')
+    
+    subplot(323)
+    histogram(err_pitch-mean(err_pitch))
+    xlabel('pitch err')
+    
+    subplot(325)
+    histogram(err_roll-mean(err_roll))
+    xlabel('roll err')
+    
+    subplot(322)
+    histogram(err_x-mean(err_x))
+    xlabel('x err')
+    
+    subplot(324)
+    histogram(err_y-mean(err_y))
+    xlabel('y err')
+    
+    subplot(326)
+    histogram(err_z-mean(err_z))
+    xlabel('z err')
+end
