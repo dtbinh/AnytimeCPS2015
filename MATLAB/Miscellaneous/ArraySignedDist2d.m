bc3f4a496b5325a5ea777d0d1391fdfb637a4b27
Yash Vardhan Pant <yashpant@seas.upenn.edu>
2016-07-21 17:11:45 -0400

    mexican hat wavelet for R2

--- MATLAB/Miscellaneous/ArraySignedDist2d.m
+++ MATLAB/Miscellaneous/ArraySignedDist2d.m
@@ -1,10 +1,16 @@
 P = Polyhedron('lb',[-1 -1],'ub',[1 1]);
+xmax = 10;
+xmin = -10;
+ymax = 10;
+ymin = -10;
+dx = 0.05;
+dy = 0.05;
 
-grid_x = -2:.05:2;
-grid_y = -2:.05:2;
+grid_x = xmin:dx:xmax;
+grid_y = ymin:dy:ymax;
 tic;
 dist_array_xy = zeros(numel(grid_x),numel(grid_y));
-
+%%
 figure;
 
 for i = 1:numel(grid_x)
@@ -25,3 +31,7 @@ tic
 a = arrayfun(@(x,y) getSignedDistance([x;y],P), msg_x(:), msg_y(:));
 dist_array_xy_vec = reshape(a,numel(grid_x),numel(grid_y));
 t_vect = toc;
+%% mex hat on the grid
+[msg_x,msg_y] = meshgrid(grid_x,grid_y);
+a = arrayfun(@(x,y) mexHatWav([x;y],1), msg_x(:), msg_y(:));
+mexhat = reshape(a,numel(grid_x),numel(grid_y));
