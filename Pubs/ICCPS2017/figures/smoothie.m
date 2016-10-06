b0a837d149cdb2afa904480ec95b2672bc9cfdf1
Houssam Abbas <habbas@seas.upenn.edu>
2016-10-06 03:22:56 -0400

    Lots of writing

---
+++ Pubs/ICCPS2017/figures/smoothie.m
@@ -0,0 +1,64 @@
+function smoothie
+n = 2;
+A = [0 1
+    0 -1
+    1 0
+    -1 0
+    ];
+b = [1; 1; 1; 1];
+% Square is centered at 0 and extends along the x-axis from S(1) to S(2)
+% and along the y-axis from S(3) to S(4)
+S = [-1, 1, -1, 1];
+dx = 0.1;
+B = 2;
+N = length(-B:dx:B);
+i=N;j=1;
+F = zeros(N);
+Je = F;
+J = J;
+epsi = 0.01;
+for x1=-B:dx:B
+    for x2=-B:dx:B
+        xx = [x1;x2];
+        F(i,j) = distFromSquare(xx,S);
+%         F(i,j) = DistFromPolyhedra(xx,A,b);
+        Je(i,j) = (1/epsi^n)*jay(xx/epsi);
+        J(i,j) = jay(xx);
+        i = i-1;
+    end
+    j = j+1;
+    i = N;
+end
+
+Fe = conv2(F, Je, 'same');
+c = max(max(abs(F)))/max(max(abs(Fe)));
+Fe = c*Fe;
+surf(Fe); title('Smoothed dist')
+max(max(abs(F-Fe)))
+
+1;
+
+    function dist = distFromSquare(xx, S)
+        if (xx(1) >= S(1) && xx(1) <= S(2))
+            if xx(2) <= S(4) && xx(2) >= S(3)
+                dist = 0;
+            else
+                dist = min(abs([xx(2)-S(3), xx(2) - S(4)]));
+            end                            
+        elseif (xx(2) >= S(3) && xx(2) <= S(4))            
+            dist = min(abs([xx(1)-S(1), xx(1) - S(2)]));           
+        else
+            dist = norm(abs(xx)-[1;1]);
+        end
+    end
+    function y = h(x)
+        if x <= 0
+            y=0;
+        else
+            y = exp(-1/x^2);
+        end
+    end
+    function y = jay(x)        
+        y = h(1-norm(x)^2);
+    end
+end
