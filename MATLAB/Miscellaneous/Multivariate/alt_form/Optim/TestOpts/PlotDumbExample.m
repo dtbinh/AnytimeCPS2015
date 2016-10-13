a87b057c8287cd5ac50ee2621751f145c0884ae7
Yash Vardhan Pant <yashpant@seas.upenn.edu>
2016-10-12 22:26:35 -0400

    made the changes, edited figure 2

--- MATLAB/Miscellaneous/Multivariate/alt_form/Optim/TestOpts/PlotDumbExample.m
+++ MATLAB/Miscellaneous/Multivariate/alt_form/Optim/TestOpts/PlotDumbExample.m
@@ -3,15 +3,15 @@ load('sqp_dumbexample.mat');
 figure;
 
 hold on;
-plot(x_0(1),x_0(2),'m*','MarkerSize',10,'LineWidth',2);
+plot(x_0(1),x_0(2),'r+','MarkerSize',16,'LineWidth',2);
 hold on;
-plot(x(1),x(2),'kp','MarkerSize',10,'LineWidth',2);
+plot(x(1),x(2),'kp','MarkerSize',16,'LineWidth',2);
 hold on;
-plot(2.5,2.5,'g+','MarkerSize',10,'LineWidth',2);
+plot(2.5,2.5,'g*','MarkerSize',16,'LineWidth',2);
 
 for i = 1:size(x_samples,2)
    hold on;
-   plot(x_samples(1,i),x_samples(2,i),'o');
+   plot(x_samples(1,i),x_samples(2,i),'o','MarkerSize',10);
 end
 P_feas = Polyhedron('lb',[-2.5 -2.5],'ub',[2.5 2.5]);
 P_set = Polyhedron('lb',[-1 -1],'ub',[1 1]);
@@ -21,4 +21,3 @@ plot(P_set,'Color','red','Alpha',0.3);hold on;
 legend('x_0','x','x^*','samples')
 xlabel('x_1');
 ylabel('x_2');
-axis equal
