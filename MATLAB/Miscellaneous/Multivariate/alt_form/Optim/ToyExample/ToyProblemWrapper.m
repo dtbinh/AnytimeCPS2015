65e13ed36497e2dac655c3ce4548d37483af89fd
yashpant <pant.yash@gmail.com>
2017-02-14 18:49:37 -0500

    3 specs for toy example, SRSQP vs BS

--- MATLAB/Miscellaneous/Multivariate/alt_form/Optim/ToyExample/ToyProblemWrapper.m
+++ MATLAB/Miscellaneous/Multivariate/alt_form/Optim/ToyExample/ToyProblemWrapper.m
@@ -46,7 +46,7 @@ end
 disp('Mean, max and std. exec time for BluSTL')
 [mean(time_per_shot_BS) max(time_per_shot_BS) std(time_per_shot_BS)]
 %% save data
-save('Data/SatModeTimes_alwaysNot_40.mat','time_per_shot_SRSQP','time_per_shot_BS','ii');
+save('Data/SatModeTimes_alwaysNot_20.mat','time_per_shot_SRSQP','time_per_shot_BS','ii');
 %% SR-SQP Max Mode
 
 robustness_max = 1;
@@ -70,6 +70,21 @@ disp('Mean, max and std. exec time for SQSQP in max mode')
 save('Data/MaxModeTimes.mat','time_per_shot_SRSQP_max');
 
 %% plotting
+load('Data/TimingSat.mat');
+L = 20:10:50;
+figure;
+errorbar(L,[t_20_sr t_30_sr t_40_sr t_50_sr], ...
+    [er_20_sr er_30_sr er_40_sr er_50_sr]);
+hold all;grid on;
+errorbar(L,[t_20_bs t_30_bs t_40_bs t_50_bs], ...
+    [er_20_bs er_30_bs er_40_bs er_50_bs]);
+xlabel('Horizon Length');
+ylabel('Exec. time one shot (s)');
+legend('SR-SQP','BluSTL');
+title('Sat. always safe and eventually terminal');
+
+
+%% plotting
 L = 20:10:50;
 fname_prefix = 'Data/SatModeTimes_eventually_';
 for jj = 1:numel(L)
@@ -88,3 +103,24 @@ xlabel('Horizon Length');
 ylabel('Exec. time one shot (s)');
 legend('SR-SQP','BluSTL');
 title('Sat. eventually terminal set');
+
+
+%% plotting always not
+L = 20:10:50;
+fname_prefix = 'Data/SatModeTimes_alwaysNot_';
+for jj = 1:numel(L)
+    fname_full = strcat(fname_prefix,num2str(L(jj)));
+    load(fname_full);
+    mean_BS(jj) = mean(time_per_shot_BS);
+    er_BS(jj) = std(time_per_shot_BS);
+    mean_SRSQP(jj) = mean(time_per_shot_SRSQP);
+    er_SRSQP(jj) = std(time_per_shot_SRSQP);
+end
+figure;
+errorbar(L,mean_SRSQP,er_SRSQP);hold all;
+grid on;
+errorbar(L,mean_BS,er_BS);hold all;
+xlabel('Horizon Length');
+ylabel('Exec. time one shot (s)');
+legend('SR-SQP','BluSTL');
+title('Sat. always not unsafe set');
